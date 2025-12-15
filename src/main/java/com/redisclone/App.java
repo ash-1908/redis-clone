d487a44cf9ce12c6d2f9a7eba814ed1ba3b770b1
anmol <ash-1908>
2025-12-15 22:34:01 +0530

    initialized project

---
+++ src/main/java/com/redisclone/App.java
@@ -0,0 +1,13 @@
+package com.redisclone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
